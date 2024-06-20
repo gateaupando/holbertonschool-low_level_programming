cfc8823384832da236980fa11c4fe7209fe4f122
gateaupando <alexandrepando02@gmail.com>
2024-06-20 13:35:16 +0000

    shell-low_level

---
+++ hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - script used to print a message without using puts
+ * Description : description programme
+ * Return: always 0 (succes)
+ */
+int main (void)
+{
+	printf(with proper grammar , but the outcome is a piece of art)
+	return (0);
+}
