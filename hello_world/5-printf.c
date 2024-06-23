8a307c2ab92e2eefaa277cdc20ef535eac38372a
gateaupando <alexandrepando02@gmail.com>
2024-06-23 13:38:18 +0000

    low_level

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -4,7 +4,7 @@
  * Description : description programme
  * Return: always 0 (succes)
  */
-int main (void)
+int main(void)
 {
 	printf(\"with proper grammar , but the outcome is a piece of art,\");
 	return (0);
